33c03d636d6c9d3d74de4018ba2e8d2a4feac638
yusefHajeb <watanalihsas@gmail.com>
2023-09-12 01:14:36 +0300

    bottomNavigation with Bloc

--- lib/features/AllFeatures/presentation/pages/application_page.dart
+++ lib/features/AllFeatures/presentation/pages/application_page.dart
@@ -1,73 +1,71 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:university/core/color/app_color.dart';
 import 'package:university/core/widget/application_wedget.dart';
+import 'package:university/features/AllFeatures/presentation/bloc/lading_page/lading_page_bloc.dart';
 
-class ApplicationPage extends StatefulWidget {
-  const ApplicationPage({super.key});
+class LadingPage extends StatelessWidget {
+  const LadingPage({super.key});
 
-  @override
-  State<ApplicationPage> createState() => _ApplicationPageState();
-}
-
-class _ApplicationPageState extends State<ApplicationPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(
-          color: AppColors.white,
-        ),
-        child: SafeArea(
-          child: Scaffold(
-              body: buildPage(1),
-              bottomNavigationBar: Container(
-                  decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: AppColors.greyColor.withOpacity(0.2),
-                          spreadRadius: 1,
-                          blurRadius: 1,
-                        ),
-                      ],
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(12),
-                          topRight: Radius.circular(12))),
-                  child: BottomNavigationBar(
-                      elevation: 0,
-                      type: BottomNavigationBarType.fixed,
-                      showSelectedLabels: false,
-                      showUnselectedLabels: false,
-                      selectedItemColor: AppColors.primaryAccentColor,
-                      unselectedItemColor: AppColors.greyColor,
-                      items: [
-                        BottomNavigationBarItem(
-                            label: "Home",
-                            icon: SvgPicture.asset("assets/icons/house.svg",
-                                color: AppColors.kBGColor, width: 35.0)),
-                        BottomNavigationBarItem(
-                            label: "schedule",
-                            icon: SvgPicture.asset("assets/icons/calendar.svg",
-                                width: 35.0)),
-                        BottomNavigationBarItem(
-                            label: "homework",
-                            icon: SizedBox(
-                                width: 40,
-                                height: 40,
-                                child: SvgPicture.asset(
-                                    "assets/icons/homework.svg",
-                                    width: 35.0))),
-                        BottomNavigationBarItem(
-                            label: "book",
-                            icon: SvgPicture.asset("assets/icons/house.svg",
-                                width: 35.0)),
-                        BottomNavigationBarItem(
-                            label: "book2",
-                            icon: SvgPicture.asset("assets/icons/read_book.svg",
-                                width: 35.0)),
-                      ]))),
+    return BlocConsumer<LadingPageBloc, LadingPageState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        return SafeArea(
+            child: Scaffold(
+          bottomNavigationBar: Container(
+            decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: AppColors.greyColor.withOpacity(0.1),
+                    spreadRadius: 1,
+                    blurRadius: 1,
+                  ),
+                ],
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(20))),
+            child: BottomNavigationBar(
+              items: bottomNavItems,
+              // elevation: 0,
+              type: BottomNavigationBarType.shifting,
+              currentIndex: state.tabIndex,
+              showSelectedLabels: true,
+              // showUnselectedLabels: false,
+              selectedItemColor: Theme.of(context).colorScheme.primary,
+              unselectedItemColor: AppColors.greyColor,
+              onTap: ((value) {
+                BlocProvider.of<LadingPageBloc>(context).add(TabChange(value));
+              }),
+            ),
+          ),
+          body: buildPage(0),
         ));
+      },
+    );
   }
 }
+
+List<BottomNavigationBarItem> bottomNavItems = <BottomNavigationBarItem>[
+  BottomNavigationBarItem(
+      label: "Home",
+      icon: SvgPicture.asset("assets/icons/house.svg",
+          color: AppColors.kBGColor, width: 35.0)),
+  BottomNavigationBarItem(
+      label: "schedule",
+      icon: SvgPicture.asset("assets/icons/calendar.svg", width: 35.0)),
+  BottomNavigationBarItem(
+      label: "homework",
+      icon: SizedBox(
+          width: 40,
+          height: 40,
+          child: SvgPicture.asset("assets/icons/homework.svg", width: 35.0))),
+  BottomNavigationBarItem(
+      label: "book",
+      icon: SvgPicture.asset("assets/icons/house.svg", width: 35.0)),
+  BottomNavigationBarItem(
+      label: "book2",
+      icon: SvgPicture.asset("assets/icons/read_book.svg", width: 35.0)),
+];
