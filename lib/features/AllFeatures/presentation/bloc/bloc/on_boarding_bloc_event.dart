7e28e2b661d5a869dc3bb8e7912bdba8ea8c203f
yusefHajeb <watanalihsas@gmail.com>
2023-09-14 00:09:48 +0300

    change onbording

---
+++ lib/features/AllFeatures/presentation/bloc/bloc/on_boarding_bloc_event.dart
@@ -0,0 +1,11 @@
+part of 'on_boarding_bloc_bloc.dart';
+
+class OnBoardingBlocEvent extends Equatable {
+  // const OnBoardingBlocEvent();
+
+  // int counter;
+  OnBoardingBlocEvent();
+
+  @override
+  List<Object> get props => [];
+}
