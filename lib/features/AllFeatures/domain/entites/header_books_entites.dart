69053dc715ac3788e997f92f2ef4bc63013d23e7
yusefHajeb <watanalihsas@gmail.com>
2023-09-17 03:18:26 +0300

    header books model

---
+++ lib/features/AllFeatures/domain/entites/header_books_entites.dart
@@ -0,0 +1,45 @@
+import 'package:equatable/equatable.dart';
+
+class BookDetaile extends Equatable {
+  final int? id;
+  final int? category_id;
+  final int? patch_id;
+  final String? subject;
+  final String? img_book;
+  final String? name_book;
+  final String? write_book;
+  final String? pdfUrl;
+
+  const BookDetaile({
+    this.id,
+    this.category_id,
+    this.patch_id,
+    this.subject,
+    this.img_book,
+    this.name_book,
+    this.write_book,
+    this.pdfUrl,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        patch_id,
+        subject,
+        img_book,
+        name_book,
+        write_book,
+        category_id,
+        pdfUrl
+      ];
+}
+
+class CategoryBooks extends Equatable {
+  final String? book_title;
+  final int? book_id;
+  CategoryBooks({required this.book_title, required this.book_id});
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [book_title];
+}
