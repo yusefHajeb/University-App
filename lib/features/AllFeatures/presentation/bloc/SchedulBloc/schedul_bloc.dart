566034130697060f2f0356972e1f7328a1ae35ef
yusefHajeb <watanalihsas@gmail.com>
2023-10-26 22:32:54 +0300

    the bloc fetch data from 2 table from db,

--- lib/features/AllFeatures/presentation/bloc/SchedulBloc/schedul_bloc.dart
+++ lib/features/AllFeatures/presentation/bloc/SchedulBloc/schedul_bloc.dart
@@ -3,6 +3,8 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:university/features/AllFeatures/domain/usecase/ScheduleUsecae/get_all_schedule.dart';
+import 'package:university/features/AllFeatures/domain/usecase/ScheduleUsecae/get_tody_letchers.dart';
+import 'package:university/main.dart';
 import '../../../../../core/error/failure.dart';
 import '../../../../../core/function/failure_to_message.dart';
 import '../../../domain/entites/schedule.dart';
@@ -10,11 +12,14 @@ part 'schedul_event.dart';
 part 'schedul_state.dart';
 
 late Either<Failure, List<Schedule>> schedulOrError;
+late Either<Failure, List<Schedule>> newletchersOrOld;
 
 class ScheduleBloc extends Bloc<SchedulEvent, ScheduleState> {
   final GetAllScheduleUsecase getAllScheduleUsecase;
+  final GetLetchersUsecase getLetchersUsecase;
   // final GetNotificationScheduleUsecase schedlulenotificationUsecae;
-  ScheduleBloc({required this.getAllScheduleUsecase})
+  ScheduleBloc(
+      {required this.getAllScheduleUsecase, required this.getLetchersUsecase})
       : super(SchedulInitial()) {
     on<SelectDayScheduleEvent>(_selectDayScheduleEvent);
     on<GetAllScheduleEvent>(_getAllScheduleEvent);
@@ -56,7 +61,17 @@ class ScheduleBloc extends Bloc<SchedulEvent, ScheduleState> {
     SelectDayScheduleEvent event,
     Emitter<ScheduleState> emit,
   ) async {
-    emit(_failureOrSchedualToState(schedulOrError, event.index, event.day));
+    if (event.index > DateTime.now().day) {
+      emit(_failureOrSchedualToState(schedulOrError, event.index, event.day));
+    } else {
+      emit(
+        _failureOrSchedualToState(
+          newletchersOrOld,
+          event.index,
+          event.day,
+        ),
+      );
+    }
   }
 
   Future<void> _getAllScheduleEvent(
@@ -68,23 +83,58 @@ class ScheduleBloc extends Bloc<SchedulEvent, ScheduleState> {
     final now = DateTime.now();
     final currentHour = now.hour;
     print("hour is $currentHour");
-    if (currentHour > 14) {
-      final tomorrow = now.add(Duration(days: 1));
-      print("tomorrow is ");
-      print(DateFormat('EEEE', 'ar').format(tomorrow));
+    // final tomorrow = now.add(const Duration(days: 1));
+    newletchersOrOld = await getLetchersUsecase();
+    if (currentHour >= 17) {
+      final tomorrow = now.add(const Duration(days: 1));
+      print("curent Houwer <17");
+      if (await socket.connected) {
+        print("socket =====");
+        // newletchersOrOld = await getLetchersUsecase();
+        emit(_failureOrSchedualToState(
+            newletchersOrOld,
+            int.parse(DateFormat('dd').format(tomorrow)),
+            DateFormat('EEEE', 'ar').format(tomorrow)));
+      } else {
+        print("no socket");
+
+        print(DateFormat('EEEE', 'ar').format(tomorrow));
+        print("today is :");
+        print(DateFormat('EEEE', 'ar').format(tomorrow));
+        emit(_failureOrSchedualToState(
+            schedulOrError,
+            int.parse(DateFormat('dd').format(tomorrow)),
+            DateFormat('EEEE', 'ar').format(tomorrow)));
+      }
+    } else {
+      print("no socket");
+      print(DateFormat('EEEE', 'ar').format(now));
       print("today is :");
       print(DateFormat('EEEE', 'ar').format(now));
-
       emit(_failureOrSchedualToState(
           schedulOrError,
-          int.parse(DateFormat('dd').format(tomorrow)),
-          DateFormat('EEEE', 'ar').format(tomorrow)));
-    } else {
-      emit(_failureOrSchedualToState(
-          schedulOrError,
-          int.parse(DateFormat('dd').format(DateTime.now())),
+          int.parse(DateFormat('dd').format(now)),
           DateFormat('EEEE', 'ar').format(now)));
     }
+    //  else if (currentHour > 17) {
+    //   print("tomorrow is ");
+    //   print(DateFormat('EEEE', 'ar').format(tomorrow));
+    //   print("today is :");
+    //   print(DateFormat('EEEE', 'ar').format(now));
+    //   emit(_failureOrSchedualToState(
+    //       newletchersOrOld,
+    //       int.parse(DateFormat('dd').format(now)),
+    //       DateFormat('EEEE', 'ar').format(now)));
+    // } else {
+    //   emit(_failureOrSchedualToState(
+    //       newletchersOrOld,
+    //       int.parse(DateFormat('dd').format(DateTime.now())),
+    //       DateFormat('EEEE', 'ar').format(now)));
+    //   //   emit(_failureOrSchedualToState(
+    //   // schedulOrError,
+    //   // int.parse(DateFormat('dd').format(DateTime.now())),
+    //   // DateFormat('EEEE', 'ar').format(now)));
+    // }
   }
 
   Future<void> _refreshScheduleEvent(
